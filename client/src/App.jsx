0398c61802a74ee89c818787963d547046577fb4
Bhut-Meet <meetbhut.7272@gmail.com>
2024-06-24 16:15:52 +0530

    Initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+// import { useState } from 'react'
+// import './App.css'
+import {BrowserRouter,Routes, Route} from 'react-router-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Users from './Users'
+import CreateUser from './CreateUser'
+import UpdateUser from './UpdateUser'
+
+function App() {
+
+  return (
+    <div>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Users></Users>}></Route>
+          <Route path='/create' element={<CreateUser></CreateUser>}></Route>
+          <Route path='/update/:id' element={<UpdateUser></UpdateUser>}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  )
+}
+
+export default App
---
+++ client/src/Users.jsx
@@ -0,0 +1,57 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+export default function Users() {
+    const [users, setUsers] = useState([]);
+
+    useEffect(() => {
+        axios.get('http://localhost:3001')
+            .then(result => setUsers(result.data))
+            .catch(err => console.log(err));
+    }, []);
+
+    const handleDelete = (id) => {
+        axios.delete("http://localhost:3001/deleteUser/" + id)
+            .then(res => {
+                console.log(res);
+                window.location.reload();
+            })
+            .catch(err => console.log(err));
+    }
+
+    return (
+        <div className="d-flex vh-auto bg-primary justify-content-center align-items-center">
+            <div className='w-50 bg-white rounded p-3 m-3'>
+                <Link to="/create" className='btn btn-success mb-3'>Add user</Link>
+                <table className='table table-dark text-center'>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Email</th>
+                            <th>Age</th>
+                            <th>Password</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            users.map((user) => (
+                                <tr key={user._id}>
+                                    <td>{user.name}</td>
+                                    <td>{user.email}</td>
+                                    <td>{user.age}</td>
+                                    <td>{user.password}</td> {/* Placeholder for password */}
+                                    <td>
+                                        <Link to={`/update/${user._id}`} className='btn btn-success'>Update</Link> &nbsp;
+                                        <button className='btn btn-danger' onClick={() => handleDelete(user._id)}>Delete</button>
+                                    </td>
+                                </tr>
+                            ))
+                        }
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+}
