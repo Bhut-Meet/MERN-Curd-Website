d46b2812e3b5ada5777f4e358e033d69639c5b85
Bhut-Meet <meetbhut.7272@gmail.com>
2024-06-24 16:25:59 +0530

    new

--- server/index.js
+++ server/index.js
@@ -9,6 +9,17 @@ app.use(express.json())
 // Connect to MongoDB database
 mongoose.connect("mongodb://127.0.0.1:27017/crud")
 
+const corsOptions={
+    // origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "mern-curd-website.vercel.app"],
+    // origin: "https://websitedevelopmentservices.vercel.app",
+    // origin:"https://websitedevelopmentservices.vercel.app", // Replace with allowed origins from environment variable or default to Vercel domain
+    methods: ["GET", "HEAD","PUT","PATCH","POST","DELETE"],
+    Credential:true,
+}
+
+app.use(cors(corsOptions));
+
 app.get("/", (req,res) => {
     UserModel.find({})
     .then(users => res.json(users))
