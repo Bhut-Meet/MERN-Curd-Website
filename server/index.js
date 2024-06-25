1472247be60cb6be456e6325cdc8c4c1c93767b5
Bhut-Meet <meetbhut.7272@gmail.com>
2024-06-25 10:46:44 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -29,29 +29,29 @@ const corsOptions={
 }
 
 app.use(cors(corsOptions));
-app.use(cors(corsMiddleware))
+app.use(cors())
 
-app.get("/", (req,res) => {
+app.get("/", corsMiddleware, (req,res) => {
     UserModel.find({})
     .then(users => res.json(users))
     .catch(err => res.json(err))
 })
 
-app.get("/getUser/:id", (req,res) => {
+app.get("/getUser/:id", corsMiddleware,  (req,res) => {
     const id = req.params.id;
     UserModel.findById({_id:id})
     .then(users => res.json(users))
     .catch(err => res.json(err))
 })
 
-app.put("/updateUser/:id", (req,res) => {
+app.put("/updateUser/:id", corsMiddleware,  (req,res) => {
     const id = req.params.id;
     UserModel.findByIdAndUpdate({_id:id},{name:req.body.name, email:req.body.email, age:req.body.age})
     .then(users => res.json(users))
     .catch(err => res.json(err))
 })
 
-app.delete("/deleteUser/:id", (req,res) => {
+app.delete("/deleteUser/:id", corsMiddleware,  (req,res) => {
     const id = req.params.id;
     UserModel.findByIdAndDelete({_id:id})
     .then(res => res.json(res))
@@ -59,7 +59,7 @@ app.delete("/deleteUser/:id", (req,res) => {
 })
 
 
-app.post("/createUser", (req,res) => {
+app.post("/createUser", corsMiddleware, (req,res) => {
     UserModel.create(req.body)
     .then(users => res.json(users))
     .catch(err => res.json(err))
