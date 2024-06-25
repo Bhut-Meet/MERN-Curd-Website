fb455683b2967cbe45455f6d303416d63aee3683
Bhut-Meet <meetbhut.7272@gmail.com>
2024-06-25 14:53:30 +0530

    Update Users.jsx

--- client/src/Users.jsx
+++ client/src/Users.jsx
@@ -38,47 +38,72 @@ export default function Users() {
             </div>
             );
         }
-    return (
-        <div className="container-fluid">
-            <div className="row justify-content-center mt-3">
-                <div className='col-md-8'>
-                    <div className='card'>
-                        <div className='card-header bg-primary text-white'>
-                            <h5 className='card-title'>User Management</h5>
+
+
+        if (users.length === 0) {
+            return (
+                <div className="container-fluid">
+                    <div className="row justify-content-center mt-3">
+                        <div className='col-md-8'>
+                            <div className='card'>
+                                <div className='card-header bg-primary text-white'>
+                                    <h5 className='card-title'>User Management</h5>
+                                </div>
+                                <div className='card-body'>
+                                    <Link to="/create" className='btn btn-success mb-3'>Add User</Link>
+                                    <div className='text-center'>
+                                        <p>No users found.</p>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        <div className='card-body'>
-                            <Link to="/create" className='btn btn-success mb-3'>Add User</Link>
-                            <div className='table-responsive'>
-                                <table className='table table-dark table-bordered table-striped text-center'>
-                                    <thead>
-                                        <tr>
-                                            <th>Name</th>
-                                            <th>Email</th>
-                                            <th>Age</th>
-                                            <th>Password</th> {/* Placeholder for password */}
-                                            <th>Action</th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {users.map((user) => (
-                                            <tr key={user._id}>
-                                                <td>{user.name}</td>
-                                                <td>{user.email}</td>
-                                                <td>{user.age}</td>
-                                                <td>{user.password}</td> {/* Placeholder for password */}
-                                                <td>
-                                                    <Link to={`/update/${user._id}`} className='btn btn-success btn-sm me-2'>Update</Link>
-                                                    <button className='btn btn-danger btn-sm' onClick={() => handleDelete(user._id)}>Delete</button>
-                                                </td>
+                    </div>
+                </div>
+            );
+        }
+    
+        return (
+            <div className="container-fluid">
+                <div className="row justify-content-center mt-3">
+                    <div className='col-md-8'>
+                        <div className='card'>
+                            <div className='card-header bg-primary text-white'>
+                                <h5 className='card-title'>User Management</h5>
+                            </div>
+                            <div className='card-body'>
+                                <Link to="/create" className='btn btn-success mb-3'>Add User</Link>
+                                <div className='table-responsive'>
+                                    <table className='table table-dark table-bordered table-striped text-center'>
+                                        <thead>
+                                            <tr>
+                                                <th>Name</th>
+                                                <th>Email</th>
+                                                <th>Age</th>
+                                                <th>Password</th> {/* Placeholder for password */}
+                                                <th>Action</th>
                                             </tr>
-                                        ))}
-                                    </tbody>
-                                </table>
+                                        </thead>
+                                        <tbody>
+                                            {users.map((user) => (
+                                                <tr key={user._id}>
+                                                    <td>{user.name}</td>
+                                                    <td>{user.email}</td>
+                                                    <td>{user.age}</td>
+                                                    <td>{user.password}</td> {/* Placeholder for password */}
+                                                    <td>
+                                                        <Link to={`/update/${user._id}`} className='btn btn-success btn-sm me-2'>Update</Link>
+                                                        <button className='btn btn-danger btn-sm' onClick={() => handleDelete(user._id)}>Delete</button>
+                                                    </td>
+                                                </tr>
+                                            ))}
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    );
-}
+        );
+    }
+
